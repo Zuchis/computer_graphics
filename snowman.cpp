39923f9aaa7061324126575ec17b79cd87a7c1ce
Zuchis <liquimaster@gmail.com>
2015-09-10 01:50:02 -0300

    This got to be the lamest snowman I have ever seen, don't open it in front of your friends if you don't want to die of embarassment

--- snowman.cpp
+++ snowman.cpp
@@ -1,8 +1,44 @@
+#include <cstdlib>
 #include <GL/freeglut.h>
 
 static void drawDaSnowman()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    //main
+    glPushMatrix();
+            glTranslatef(0.0, 0.0, 0.75);
+            glColor3f(1.0, 1.0, 1.0);
+            glPushMatrix();
+                    glTranslatef(0.0, 0.0, 0.75);
+                    glutSolidSphere(0.75, 20, 20);
+            glPopMatrix();
+        //head
+        glPushMatrix();
+                glTranslatef(0.0, 0.0, 1.75); 
+                glutSolidSphere(0.25, 20, 20); 
+
+                // Eyes 
+                glColor3f(0.0, 0.0, 0.0);
+                glPushMatrix();
+                        glTranslatef(0.0, -0.18, 0.10); 
+                        glPushMatrix();
+                                glTranslatef(-0.05, 0.0, 0.0);
+                                glutSolidSphere(0.05, 10, 10); // right eye
+                        glPopMatrix();
+                        glPushMatrix();
+                                glTranslatef(+0.05, 0.0, 0.0);
+                                glutSolidSphere(0.05, 10, 10); // left eye
+                        glPopMatrix();
+                glPopMatrix();
+
+                // Nose 
+                glColor3f(1.0, 0.5, 0.5); 
+                glPushMatrix();
+                        glRotatef(90.0, 1.0, 0.0, 0.0); 
+                        glutSolidCone(0.08, 0.5, 10, 2); 
+                glPopMatrix();
+        glPopMatrix();
+    glPopMatrix();
     glutSwapBuffers();
 }
 
@@ -14,7 +50,7 @@ int main (int argc, char **argv)
     glutInitWindowPosition(100,100);
     glutCreateWindow("Snowman Bolado");
     glutDisplayFunc(drawDaSnowman);
-    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    //glClearColor(0.0f,0.0f,0.0f,0.0f);
     glutMainLoop();
     
     return 0;
