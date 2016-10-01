db9b1c73ab3482fd98960ae4a4d46c918c0b9d67
Zuchis <liquimaster@gmail.com>
2016-10-01 16:36:47 +0100

    updated assignment

--- assignments/assignment1/main.cpp
+++ assignments/assignment1/main.cpp
@@ -35,31 +35,31 @@ void cleanup()
 
 void display()
 {
-	++FrameCount;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Draw something
-	glutSwapBuffers();
+    ++FrameCount;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Draw something
+    glutSwapBuffers();
 }
 
 void idle()
 {
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 void reshape(int w, int h)
 {
-	WinX = w;
-	WinY = h;
-	glViewport(0, 0, WinX, WinY);
+    WinX = w;
+    WinY = h;
+    glViewport(0, 0, WinX, WinY);
 }
 
 void timer(int value)
 {
-	std::ostringstream oss;
-	oss << CAPTION << ": " << FrameCount << " FPS @ (" << WinX << "x" << WinY << ")";
-	std::string s = oss.str();
-	glutSetWindow(WindowHandle);
-	glutSetWindowTitle(s.c_str());
+    std::ostringstream oss;
+    oss << CAPTION << ": " << FrameCount << " FPS @ (" << WinX << "x" << WinY << ")";
+    std::string s = oss.str();
+    glutSetWindow(WindowHandle);
+    glutSetWindowTitle(s.c_str());
     FrameCount = 0;
     glutTimerFunc(1000, timer, 0);
 }
@@ -68,81 +68,81 @@ void timer(int value)
 
 void setupCallbacks() 
 {
-	glutCloseFunc(cleanup);
-	glutDisplayFunc(display);
-	glutIdleFunc(idle);
-	glutReshapeFunc(reshape);
-	glutTimerFunc(0,timer,0);
+    glutCloseFunc(cleanup);
+    glutDisplayFunc(display);
+    glutIdleFunc(idle);
+    glutReshapeFunc(reshape);
+    glutTimerFunc(0,timer,0);
 }
 
 void checkOpenGLInfo()
 {
-	const GLubyte *renderer = glGetString(GL_RENDERER);
-	const GLubyte *vendor = glGetString(GL_VENDOR);
-	const GLubyte *version = glGetString(GL_VERSION);
-	const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
-	std::cerr << "OpenGL Renderer: " << renderer << " (" << vendor << ")" << std::endl;
-	std::cerr << "OpenGL version " << version << std::endl;
-	std::cerr << "GLSL version " << glslVersion << std::endl;
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *version = glGetString(GL_VERSION);
+    const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    std::cerr << "OpenGL Renderer: " << renderer << " (" << vendor << ")" << std::endl;
+    std::cerr << "OpenGL version " << version << std::endl;
+    std::cerr << "GLSL version " << glslVersion << std::endl;
 }
 
 void setupOpenGL() 
 {
-	checkOpenGLInfo();
-	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glDepthMask(GL_TRUE);
-	glDepthRange(0.0, 1.0);
-	glClearDepth(1.0);
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
-	glFrontFace(GL_CCW);
+    checkOpenGLInfo();
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDepthMask(GL_TRUE);
+    glDepthRange(0.0, 1.0);
+    glClearDepth(1.0);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
 }
 
 void setupGLEW() 
 {
-	glewExperimental = GL_TRUE;
-	GLenum result = glewInit() ; 
-	if (result != GLEW_OK) {
-		std::cerr << "ERROR glewInit: " << glewGetString(result) << std::endl;
-		exit(EXIT_FAILURE);
-	} 
-	GLenum err_code = glGetError();
+    glewExperimental = GL_TRUE;
+    GLenum result = glewInit() ; 
+    if (result != GLEW_OK) {
+        std::cerr << "ERROR glewInit: " << glewGetString(result) << std::endl;
+        exit(EXIT_FAILURE);
+    } 
+    GLenum err_code = glGetError();
 }
 
 void setupGLUT(int argc, char* argv[])
 {
-	glutInit(&argc, argv);
-	
-	glutInitContextVersion(3, 3);
-	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
-	glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInit(&argc, argv);
 
-	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,GLUT_ACTION_GLUTMAINLOOP_RETURNS);
-	
-	glutInitWindowSize(WinX, WinY);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	WindowHandle = glutCreateWindow(CAPTION);
-	if(WindowHandle < 1) {
-		std::cerr << "ERROR: Could not create a new rendering window." << std::endl;
-		exit(EXIT_FAILURE);
-	}
+    glutInitContextVersion(3, 3);
+    glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+
+    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
+    glutInitWindowSize(WinX, WinY);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    WindowHandle = glutCreateWindow(CAPTION);
+    if(WindowHandle < 1) {
+        std::cerr << "ERROR: Could not create a new rendering window." << std::endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
 void init(int argc, char* argv[])
 {
-	setupGLUT(argc, argv);
-	setupGLEW();
-	setupOpenGL();
-	setupCallbacks();
+    setupGLUT(argc, argv);
+    setupGLEW();
+    setupOpenGL();
+    setupCallbacks();
 }
 
 int main(int argc, char* argv[])
 {
-	init(argc, argv);
-	glutMainLoop();	
-	exit(EXIT_SUCCESS);
+    init(argc, argv);
+    glutMainLoop();	
+    exit(EXIT_SUCCESS);
 }
 
 ///////////////////////////////////////////////////////////////////////
