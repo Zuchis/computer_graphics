4d3a9926dbb30d2dfb30bd8d1b4ada33ee2ff8a7
Zuchis <liquimaster@gmail.com>
2016-10-06 15:09:53 +0100

    finished the core of the vector class

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -1,3 +1,82 @@
 #include <iostream>
 #include <cmath>
 #include "vector.h"
+
+Vector Vector::operator + (Vector v){
+    return Vector(x + v.x,
+                    y + v.y,
+                    z + v.z);
+}
+
+Vector Vector::operator + (double k){
+    return Vector(x + k,
+                    y + k,
+                    z + k);
+}
+
+Vector Vector::operator - (Vector v){
+    return Vector(x - v.x,
+                    y - v.y,
+                    z - v.z);
+}
+
+Vector Vector::operator - (double k){
+    return Vector(x - k,
+                    y - k,
+                    z - k);
+}
+
+Vector Vector::operator / (double k){
+    return Vector(x / k,
+                    y / k,
+                    z / k);
+}
+
+Vector Vector::operator * (double k){
+    return Vector(x * k,
+                    y * k,
+                    z * k);
+}
+
+Vector Vector::operator = (Vector v){
+    return Vector(v.x,v.y,v.z);
+}
+
+bool Vector::operator == (const Vector &u){
+    return (x == u.x &&
+            y == u.y &&
+            z == u.z);
+}
+
+bool Vector::operator != (const Vector &u){
+    return !(*this == u);
+}
+
+double Vector::norm(){
+    return sqrt(x * x + y * y + z * z);
+}
+
+Vector Vector::copy(){
+    return Vector(x, y, z);
+}
+
+void Vector::normalize(){
+    *this = *this / norm();
+    return;
+}
+
+double Vector::dot (Vector v){
+    return (x * v.x +
+            y * v.y +
+            z * v.z);
+}
+
+Vector Vector::cross (Vector v){
+    return Vector(y * v.z - z * v.y,
+                    z * v.x - x * v.z,
+                    x * v.y - y * v.x);
+}
+
+void Vector::print(){
+    std::cout << "( " << x << ", " << y << ", " << z << " )" << std::endl;
+}
--- vectors/vector.h
+++ vectors/vector.h
@@ -7,7 +7,7 @@ class Vector{
     public:
         double x, y, z;
 
-        Vector(double x1 = 0, y1 = 0, z1 = 0){
+        Vector(double x1 = 0, double y1 = 0, double z1 = 0){
             x = x1;
             y = y1;
             z = z1;
@@ -20,12 +20,12 @@ class Vector{
         Vector operator / (double k);
         Vector operator * (double k);
         Vector operator = (Vector v);
-        bool operator == (const Vector &v, const Vector &u);
-        bool operator != (const Vector &v, const Vector &u);
+        bool operator == (const Vector &u);
+        bool operator != (const Vector &u);
         double dot (Vector v);
         double norm();
         Vector copy();
-        Vector cross(Vector u);
+        Vector cross(Vector v);
         void normalize();
         void print();
 };
