5ec631edfa47a499f55f4cd94360001e58bebf52
Zuchis <liquimaster@gmail.com>
2016-10-07 12:04:41 +0100

    added new commands to test the class

--- vectors/tester.cpp
+++ vectors/tester.cpp
@@ -6,8 +6,14 @@ int main (){
     Vector u = Vector(1.0,3.0,5.0);
 
     Vector s = v + u;
+    Vector *w = new Vector();
 
-    std::cout << "the value of s is: ";
+    std::cout << "v: ";
+    v.print();
+    std::cout << "u: ";
+    u.print();
+
+    std::cout << "the value of s is v + u, which is: ";
     s.print();
 
     if (v == u){
@@ -15,8 +21,43 @@ int main (){
     }
 
     if (v != u){
+        std::cout << "Testing v != u: ";
         std::cout << "v and u are not equal" << std::endl;
     }
 
+    *w = s.copy();
+    std::cout << "w is a copy of s: ";
+    w->print();
+
+    std::cout << "s - 1: ";
+    (s - 1).print();
+
+    std::cout << "s + 1: ";
+    (s + 1).print();
+
+    std::cout << "s * 2: ";
+    (s * 2).print();
+
+    std::cout << "s / 2: ";
+    (s / 2).print();
+
+    std::cout << std::endl;
+
+    std::cout << "s - u: ";
+    (s - u).print();
+
+    std::cout << "v's norm: " << v.norm() << std::endl;
+
+    std::cout << "v normalized: ";
+    v.normalized().print();
+
+    std::cout << "v dot u: " << v.dot(u) << std::endl;
+
+    std::cout << "v cross u: ";
+    (v.cross(u)).print();
+
+
+    delete w;
+        
     return 0;
 }
