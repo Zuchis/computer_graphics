bd11ae1fc77345e92470146df89e656b0ca53ce4
Zuchis <liquimaster@gmail.com>
2016-10-09 20:03:12 +0100

    added vector 2d headers

--- vectors/vector.h
+++ vectors/vector.h
@@ -31,4 +31,32 @@ class Vector{
         void print();
 };
 
+class Vector2{
+    private:
+
+    public:
+        double x, y;
+
+        Vector2(double x1 = 0, double y1 = 0){
+            x = x1;
+            y = y1;
+        }
+
+        Vector2 operator + (Vector2 v);
+        Vector2 operator + (double k);
+        Vector2 operator - (Vector2 v);
+        Vector2 operator - (double k);
+        Vector2 operator / (double k);
+        Vector2 operator * (double k);
+        Vector2 operator = (Vector2 v);
+        bool operator == (const Vector2 &u);
+        bool operator != (const Vector2 &u);
+        double norm();
+        Vector2 copy();
+        double dot (Vector2 v);
+        void normalize();
+        Vector2 normalized();
+        void print();
+}
+
 #endif
