b4daf3694fcfceaec299e333c35d16fb56996a9d
Zuchis <liquimaster@gmail.com>
2016-10-11 14:40:53 +0100

    working on the matrix class

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,7 +1,23 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-class Matrix{
+class Matrix2{
+    private:
+        double *matrix;
+        int width = 2;
+        int height = 2;
+
+    public:
+        Matrix2(){
+            matrix = new double[width * height];
+            int i, j;
+            for(i = 0; i < height; i++){
+                for(j = 0; j < width; j++){
+                    matrix[i + j * width] = 0.0;
+                }
+            }
+        }
+        
 };
 
 
