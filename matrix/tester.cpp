50fbb5958c622f130fc7cd90cd0c104dd10935ae
Zuchis <liquimaster@gmail.com>
2016-10-13 02:33:23 +0100

    added some new demos

--- matrix/tester.cpp
+++ matrix/tester.cpp
@@ -7,4 +7,18 @@ int main () {
    double values[4] = {1,2,3,4};
    Matrix2 n = Matrix2(values);
    n.print();
+   std::vector<double> r = n.row(0);
+   std::cout << std::endl;
+
+   for(auto i = r.begin(); i != r.end(); i++)
+       std::cout << *i << ' ';
+   std::cout << std::endl;
+
+   r = n.column(0);
+   for(auto i = r.begin(); i != r.end(); i++)
+       std::cout << *i << ' ';
+   std::cout << std::endl;
+
+   (n.inverted()).print();
+   
 }
