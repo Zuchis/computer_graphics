cf397d49b9be5e7c95758a13ab87ee12a055aee8
Zuchis <liquimaster@gmail.com>
2016-10-21 00:06:37 +0100

    made some utility matrices on the matrixfactory class

--- engine_project/src/matrixfactory.h
+++ engine_project/src/matrixfactory.h
@@ -11,6 +11,23 @@ class MatrixFactory{
         static Matrix3 Create3DIdentity();
         static Matrix4 Create4DIdentity();
 
+        static Matrix2 Create2DRotation(double angle);
+        static Matrix3 Create3DZRotation(double angle);
+        static Matrix3 Create3DYRotation(double angle);
+        static Matrix3 Create3DXRotation(double angle);
+
+        static Matrix3 Create3DTranslation(double XTranslation, double YTranslation);
+        static Matrix4 Create4DTranslation(double XTranslation, double YTranslation, double ZTranslation);
+
+        static Matrix2 Create2DScalingMatrix(double XFactor, double YFactor);
+        static Matrix3 Create3DScalingMatrix(double XFactor, double YFactor, double ZFactor);
+
+        static Matrix3 CONVERT_4D_TO_3D_MATRIX(Matrix4 m);
+        static Matrix2 CONVERT_3D_TO_2D_MATRIX(Matrix3 m);
+
+        static Matrix3 CONVERT_2D_TO_3D_MATRIX(Matrix2 m);
+        static Matrix4 CONVERT_3D_TO_4D_MATRIX(Matrix3 m);
+
 };
 
 
--- engine_project/src/tester_matrixf.cpp
+++ engine_project/src/tester_matrixf.cpp
@@ -4,5 +4,9 @@
 int main(){
     Matrix2 m = MatrixFactory::Create2DIdentity();
     m.print();
+
+    Matrix2 rot = MatrixFactory::Create2DRotation(90.0);
+    rot.print();
+
     return 0;
 }
